d69eb39f60020ebc568d742b2fa822007e3db78c
Brian aguiar <briantbini@outlook.com>
2021-05-03 11:21:54 -0300

    Escopo criado e metodos de exibição

---
+++ ProdutoFi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProdutoFi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
